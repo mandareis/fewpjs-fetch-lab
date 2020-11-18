a4f2b0652e300d8377ac438291e6fef77fa785ad
mandareis <amanda@depaula.xyz>
2020-11-18 08:21:40 -0800

    commit tree 41fcd845c399f0135d5a06910d60e3890cda067f

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 function fetchBooks() {
-
+  return fetch("https://anapioficeandfire.com/api/books")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (responseJson) {
+      renderBooks(responseJson);
+    });
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main')
-  books.forEach(book => {
-    const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
-    main.appendChild(h2)
-  })
+  const main = document.querySelector("main");
+  books.forEach((book) => {
+    const h2 = document.createElement("h2");
+    h2.innerHTML = book.name;
+    main.appendChild(h2);
+  });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks()
-})
+document.addEventListener("DOMContentLoaded", function () {
+  fetchBooks();
+});
